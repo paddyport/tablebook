5bbf09ee08809c86a0c2b179d1f580a3871f820f
MinamiTazz <>
2020-12-06 23:03:56 +0900

    修正

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="App"
+    :class="[
+      2<=now.getMonth()&&now.getMonth()<5 ? 'spr' : 
+      5<=now.getMonth()&&now.getMonth()<8 ? 'smm' : 
+      8<=now.getMonth()&&now.getMonth()<11 ? 'atm' : 'wnt']">
+    <LoaderLayer v-if="loaderFlg"></LoaderLayer>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Dexie from 'dexie'
+import LoaderLayer from './components/LoaderLayer'
 
 export default {
-  name: 'App',
+  name: '#App',
+  data() {
+    return {
+      loaderFlg: true,
+      db: "",
+      dbName: "tablebookDB",
+      now: "",
+    }
+  },
   components: {
-    HelloWorld
-  }
+    LoaderLayer,
+  },
+  created: function(){
+    this.now = new Date();
+    this.hiddenLoader();
+  },
+  methods: {
+    shownLoader() {
+      this.loaderFlg = true;
+    },
+    hiddenLoader() {
+      this.loaderFlg = false;
+    },
+    createDB() {
+			this.db = new Dexie(this.dbName);
+      this.createTable();
+		},
+		createTable() {
+			this.db.version(1).stores({
+				clmidx: "++cid, *clms, *clmspan",
+        rowline: "++rid",
+      });
+      // clmidx: cid, clms:{clm*(*=number): name}, clmspan{name: [clm*, clm*, clm*]},
+      // rowline: rid, clm*, clm*, clm*, ...
+      // this.testAddDB();
+    },
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./css/app.css";
 </style>
